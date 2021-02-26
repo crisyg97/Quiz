053ec82e091f395bd74c1aaab38540e69e8f6f20
crisyg97 <cristiangaray101@gmail.com>
2021-02-26 01:10:29 -0300

    Added route question and view of question, created folder controllers

--- quiz/routes/index.js
+++ quiz/routes/index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
-const user = require('./user');
+const user = require('./controllers/user');
+const question = require('./controllers/question');
 
 module.exports = app => {
   router.get('/', (req,res)=>{
     res.render('user.pug');
   });
-
   router.post('/', user.create);
+  
+  router.get('/question', (req,res) => {
+    res.render('question.pug');
+  });
+  router.get('/question', question.randomQuestion);
 
   app.use(router);
 };
